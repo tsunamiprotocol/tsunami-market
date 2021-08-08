c8003719a920a09be66df3e60d5b47de9a11c4ad
Shankar Mata <mshankarrao1970@gmail.com>
2021-08-08 18:35:41 -0400

    setup the dataset for market place from ocean protocol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './angular-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatasetComponent } from './components/dataset/dataset.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatasetComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/dataset/dataset.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import Web3 from 'web3';
+
+@Component({
+  selector: 'app-dataset',
+  templateUrl: './dataset.component.html',
+  styleUrls: ['./dataset.component.css']
+})
+export class DatasetComponent implements OnInit {
+  datasets: any;
+  _aggregatorV3InterfaceABI: any;
+  oceanPrice: number = 1;
+  ethPrice: number = 1;
+  
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get("https://aquarius.ropsten.oceanprotocol.com/api/v1/aquarius/assets/ddo").subscribe((val: any) => {
+      this.datasets = val;
+      console.log(this.datasets);
+    })
+
+    this.httpClient.get("assets/chainLinkInterfaceABI.json").subscribe((data: any) => {
+      this._aggregatorV3InterfaceABI = data.aggregatorV3InterfaceABI;
+      this.getChainLinkData("0x8DefC4eEC0dd1B6f73e5ba10466C465c1346536E", 'E');
+    })
+  }
+
+  getChainLinkData(contractAddress: string, token: string) {
+    var web3 = new Web3("https://kovan.infura.io/v3/34139380df974a6f9e0d70bf2312fdd4");
+    const addr = contractAddress;
+    var priceFeed = new web3.eth.Contract(this._aggregatorV3InterfaceABI, addr);
+    priceFeed.methods.latestRoundData().call()
+      .then((roundData: any) => {
+        // Do something with roundData
+        this.ethPrice = roundData[1] * Math.pow(10, -8);
+      });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
